cc4297041447372aed50fa01a62762b91fe91e3f
jac wida <georgekawume@gmail.com>
2024-07-15 15:38:01 +0300

    spinner

---
+++ app/loading.js
@@ -0,0 +1,11 @@
+const { Loader2 } = require("lucide-react");
+
+const Spinner = () => {
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <Loader2 className="animate-spin md:w-32 md:h-32 w-24 h-24" />
+    </div>
+  );
+};
+
+export default Spinner;
